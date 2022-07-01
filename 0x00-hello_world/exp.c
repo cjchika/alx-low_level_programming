6676fadd96c35cc70bf69adbe19236b4c90cad15
Chijioke Chika <8pluscreative@gmail.com>
2022-07-01 20:35:23 +0000

    AA

---
+++ 0x00-hello_world/exp.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+
+	a = 50;
+	b = 100;
+	c = 23;
+	d = 10;
+
+	printf("a plus b is: %d\n", a + b);
+	printf("b divided by d is: %d\n", b / d);
+	printf("c multiplied by d is: %d\n", c * d);
+	printf("b minus a is: %d\n", b - d);
+	return (0);
+}
