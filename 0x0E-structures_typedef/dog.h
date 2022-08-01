187bcaf804ba5fdea88bc177bfa55bc0fd045614
Chijioke Chika <8pluscreative@gmail.com>
2022-08-01 18:30:04 +0000

    dog struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,16 +1,11 @@
 #ifndef _DOG_H
 #define _DOG_H
-/**
- * struct dog - dog struct
- * @name: char
- * @age: int
- * @owner: char
- */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
 
 #endif
